81f8418050b6af25b61a8ee9a849002a8427c9ef
kan862 <kanchannaregal@gmail.com>
2025-04-04 09:32:53 +0530

    savethis

---
+++ java1.js
@@ -0,0 +1,10 @@
+public class demo{
+    public static void main(string[]args){
+        stringname="abc";
+        if(args !=Null && args.length>0)
+        {
+            name = args[0]
+            System.out.println("BYEBYE",welcome to java programing!);
+        }
+    }
+}
